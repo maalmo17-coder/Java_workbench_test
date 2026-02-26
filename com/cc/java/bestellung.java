dabbf4fe8219950a5669812a10cbac78a1c90501
maalmo17-coder <maalinle17@gmail.com>
2026-02-26 10:50:07 +0000

    Variale deklarieren

---
+++ com/cc/java/bestellung.java
@@ -0,0 +1,19 @@
+package com.cc.java;
+
+public class bestellung  
+{
+
+    public static void main(String[] args) {
+// Variablen deklarieren
+        String food = "noch kein Burger gewählt";
+        String drink = "noch kein Getränk gewählt";
+        String itemNumStr;
+        String flag;
+           System.out.println(food);
+           System.out.println(drink);
+
+
+    
+}
+}
+
