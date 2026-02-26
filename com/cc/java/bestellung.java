08778b02889e14bbfba598491bb6675fc974e763
maalmo17-coder <maalinle17@gmail.com>
2026-02-26 11:13:18 +0000

    food deklarieren

--- com/cc/java/bestellung.java
+++ com/cc/java/bestellung.java
@@ -9,11 +9,20 @@ public class bestellung
         String drink = "noch kein Getränk gewählt";
         String itemNumStr;
         String flag;
-           System.out.println(food);
-           System.out.println(drink);
+           //System.out.println(food);
+           //System.out.println(drink);
+ // Bestelldialog 1 : Ausgabe
+        System.out.println("-------------------------------");
+        System.out.println("Herzlich Willkommen bei MD, Ihre Bestellung bitte!");
+        System.out.println("Hamburger(1), Cheeseburger(2) oder Chilliburger(3) ?");
+
+        // Bestelldialog 1 : Eingabe
+        itemNumStr = System.console().readLine("Ihre Wahl? : ");
+        // System.out.println(itemNumStr);  // 1 oder 2 oder 3
 
 
+    }
     
 }
-}
+
 
